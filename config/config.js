363d11b07bf650ca1185ff44c7f98933632322dd
anonymoussc <50c5ac69@opayq.com>
2015-11-27 21:49:25 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,12 @@
 /**
  * Created by anonymoussc on 27/11/15 21:16.
  */
+
+var
+    config = {
+        generator         : '',
+        workspaces        : './../../../app_components',
+        templatesDirectory: './templates'
+    };
+
+module.exports = config;
